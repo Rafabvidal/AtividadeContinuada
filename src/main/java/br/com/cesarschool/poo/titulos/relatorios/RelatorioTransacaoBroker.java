7dcf2823198f6512a1c3b2c9258d093627d50b64
rbv2 <rbv2@cesar.school>
2024-11-25 18:36:31 -0300

    PASSOU TUDOOO

--- src/main/java/br/com/cesarschool/poo/titulos/relatorios/RelatorioTransacaoBroker.java
+++ src/main/java/br/com/cesarschool/poo/titulos/relatorios/RelatorioTransacaoBroker.java
@@ -3,30 +3,34 @@ package br.com.cesarschool.poo.titulos.relatorios;
 import br.com.cesarschool.poo.titulos.entidades.Transacao;
 import br.com.cesarschool.poo.titulos.repositorios.RepositorioTransacao;
 import br.com.cesarschool.poo.titulos.utils.ComparadorTransacaoPorNomeCredora;
+import br.com.cesarschool.poo.titulos.utils.Comparavel;
 import br.com.cesarschool.poo.titulos.utils.Ordenador;
 
-//Tem os dois métodos mencionados acima, que devem ler
-//do repositório de transações todas as transações e ordenar
-//o retorno desta leitura por nome da entidade credora e por
-//data hora da operação, retornando os respectivos arrays
-//ordenados.
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class RelatorioTransacaoBroker {
-    private final RepositorioTransacao repositorioTransacao;
+    private final RepositorioTransacao repositorio;
 
     public RelatorioTransacaoBroker() {
-        this.repositorioTransacao = new RepositorioTransacao();
-    }
-
-    public Transacao[] relatorioTransacaoPorNomeEntidadeCredora() {
-        Transacao[] transacoes = repositorioTransacao.buscarPorEntidadeCredora(0);
-        Ordenador.ordenar(transacoes, new ComparadorTransacaoPorNomeCredora());
-        return transacoes;
+        this.repositorio = new RepositorioTransacao();
     }
 
     public Transacao[] relatorioTransacaoPorDataHora() {
-        Transacao[] transacoes = repositorioTransacao.buscarPorEntidadeCredora(0);
-        Ordenador.ordenar(transacoes);
+        RepositorioTransacao repositorio = new RepositorioTransacao();
+        Transacao[] transacoes = repositorio.buscarTodas();
+
+        // Ordenar por dataHoraOperacao
+        Arrays.sort(transacoes, Comparator.comparing(Transacao::getDataHoraOperacao).reversed());
+        return transacoes;
+    }
+
+    public Transacao[] relatorioTransacaoPorNomeEntidadeCredora() {
+        RepositorioTransacao repositorio = new RepositorioTransacao();
+        Transacao[] transacoes = repositorio.buscarTodas();
+
+        // Ordenar por nome da entidade credora
+        Arrays.sort(transacoes, Comparator.comparing(t -> t.getEntidadeCredito().getNome()));
         return transacoes;
     }
 }
